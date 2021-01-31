fc1004f772e85d30d2f7d24bf26dd263a1185e86
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2021-01-31 19:47:23 +0300

    I added new regulations.

--- Api_And_Http_Module/axios.js
+++ Api_And_Http_Module/axios.js
@@ -1,42 +1,46 @@
-const axios=require('axios');
-const { log } = require('console');
-const myAxiosObject=axios.create({
-    baseURL:'https://restcountries.eu',
+const axios = require('axios');
+const {
+    log
+} = require('console');
+const myAxiosObject = axios.create({
+    baseURL: 'https://restcountries.eu',
     // headers:{},
     // params:{
-      
+
 
     // }
 });
-const myWeatherAxiosObject=axios.create({
-    baseURL:'api.openweathermap.org/data/2.5/weather',
+const myWeatherAxiosObject = axios.create({
+    baseURL: 'http://api.openweathermap.org/data/2.5',
     params:{
-        appid:"b83be45605eff8359f033abb75f773ff"
+        appid:"0c92f646120b88a50ff4bc27b25cb975",
+        lang:"tr"
     }
+   
 });
 
 myAxiosObject.get("rest/v2/name/turkey").then((res) => {
     console.log(res.data);
-    const ulke=res.data[0];
-    const baskent=ulke.capital;
-    const nufus=ulke.population;
-    const enlem=ulke.lating[0];
-    const boylam=ulke.lating[1];
+    const ulke = res.data[0];
+    const baskent = ulke.capital;
+    const nufus = ulke.population;
+    const enlem = ulke.latlng[0];
+    const boylam = ulke.latlng[1];
     console.log(`Nufusu : ${nufus} Enlem : ${enlem} Boylam : ${boylam} ve Başkent : ${baskent}`);
 
-    myWeatherAxiosObject.get({
-        params:{
-            q:baskent
+    myWeatherAxiosObject.get("/weather",{
+        params: {
+            q: baskent
         }
-    }).then((result) => {
-        console.log(result);
-        }).catch((err) => {
+    }).then((res) => {
+        console.log(res.data);
+        console.log(`Başkent ${res.data.name} bugün hava ${res.data.weather[0].description}`);
+    }).catch((err) => {
 
-            console.log(err);
-        });
-        
-    
+        console.log(err);
+    });
+
+
 }).catch((err) => {
     console.log(err.message);
 });
-
