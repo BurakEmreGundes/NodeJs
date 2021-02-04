afe1ddf5f74e88b973855dffe1617848f5b1171f
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2021-02-04 16:24:04 +0300

    I added new regulations.

--- Create_Web_Services_And_Express_Module/app.js
+++ Create_Web_Services_And_Express_Module/app.js
@@ -7,7 +7,6 @@ const server=http.createServer((req,res)=>{
         res.write("<h1 style='color:red'>Hello, welcome to my web services.</h1>");
         res.end();
     }else if(req.url==="/about"){
-        console.log("about page");
         res.write("<h1 style='color:blue'>You are looking for about page.</h1>");
         res.end();
     }
