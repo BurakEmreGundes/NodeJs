fc48ce2e09c761d882b3c728b7a22077f56170bf
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2021-02-04 14:47:09 +0300

    I added new regulations.

--- Api_And_Http_Module/axios.js
+++ Api_And_Http_Module/axios.js
@@ -1,46 +1,30 @@
-const axios = require('axios');
-const {
-    log
-} = require('console');
-const myAxiosObject = axios.create({
-    baseURL: 'https://restcountries.eu',
-    // headers:{},
-    // params:{
+const yargs=require('yargs');
+const getWeatherByCountry=require('./capital-weather');
 
 
-    // }
-});
-const myWeatherAxiosObject = axios.create({
-    baseURL: 'http://api.openweathermap.org/data/2.5',
-    params:{
-        appid:"0c92f646120b88a50ff4bc27b25cb975",
-        lang:"tr"
-    }
-   
-});
+calistir();
 
-myAxiosObject.get("rest/v2/name/turkey").then((res) => {
-    console.log(res.data);
-    const ulke = res.data[0];
-    const baskent = ulke.capital;
-    const nufus = ulke.population;
-    const enlem = ulke.latlng[0];
-    const boylam = ulke.latlng[1];
-    console.log(`Nufusu : ${nufus} Enlem : ${enlem} Boylam : ${boylam} ve Başkent : ${baskent}`);
 
-    myWeatherAxiosObject.get("/weather",{
-        params: {
-            q: baskent
+
+function calistir(){
+    yargs.command({
+
+        command:"ara",
+        describe:"Ulkenin başkentinin bugunki hava durumunu bulma komutu",
+        builder:{
+            ulke:{
+                describe:"Secilen ulke",
+                type:'string',
+                demandOption:true
+            }
+        }, 
+        handler(argv){
+            console.log("sea");
+            getWeatherByCountry.getWeatherByCountry(argv.ulke);
         }
-    }).then((res) => {
-        console.log(res.data);
-        console.log(`Başkent ${res.data.name} bugün hava ${res.data.weather[0].description}`);
-    }).catch((err) => {
-
-        console.log(err);
+    
     });
+    yargs.parse();
+}
 
-
-}).catch((err) => {
-    console.log(err.message);
-});
+module.exports=calistir;
--- Api_And_Http_Module/capital-weather.js
+++ Api_And_Http_Module/capital-weather.js
@@ -1,1 +1,54 @@
- 
+const axios = require('axios');
+
+const myAxiosObject = axios.create({
+    baseURL: 'https://restcountries.eu',
+    // headers:{},
+    // params:{
+
+
+    // }
+});
+
+
+const myWeatherAxiosObject = axios.create({
+    baseURL: 'http://api.openweathermap.org/data/2.5',
+    params:{
+        appid:"0c92f646120b88a50ff4bc27b25cb975",
+        lang:"tr"
+    }
+   
+});
+
+
+function getWeatherByCountry(country){
+    
+myAxiosObject.get(`rest/v2/name/${country}`).then((res) => {
+    console.log(res.data);
+    const ulke = res.data[0];
+    const baskent = ulke.capital;
+    const nufus = ulke.population;
+    const enlem = ulke.latlng[0];
+    const boylam = ulke.latlng[1];
+    console.log(`Nufusu : ${nufus} Enlem : ${enlem} Boylam : ${boylam} ve Başkent : ${baskent}`);
+
+    myWeatherAxiosObject.get("/weather",{
+        params: {
+            q: baskent
+        }
+    }).then((res) => {
+        console.log(res.data);
+        console.log(`Başkent ${res.data.name} bugün hava ${res.data.weather[0].description}`);
+    }).catch((err) => {
+
+        console.log(err);
+    });
+
+
+}).catch((err) => {
+    console.log(err.message);
+});
+}
+
+module.exports={
+    getWeatherByCountry
+}
