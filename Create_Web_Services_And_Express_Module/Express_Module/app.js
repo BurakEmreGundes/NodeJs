e1709221e0dcc44fa2ffb2b5eb0b742d4a48269f
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2021-02-04 18:27:04 +0300

    I added new regulations.

---
+++ Create_Web_Services_And_Express_Module/Express_Module/app.js
@@ -0,0 +1,54 @@
+const { response } = require("express");
+const express=require("express");
+const app=express();
+
+const kullanıcılar=[
+    {id:1,ad:'emre',yas:32},
+    {id:2,ad:'burak',yas:19},
+]
+
+
+app.get('/',(req,res)=>{
+    console.log("ana sayfaya girildi.");
+    res.send("<h1 style='color:red'>Hello, welcome to my home page.</h1>")
+});
+
+app.get("/users",(req,res)=>{
+    if(req.query.ters){
+        res.send(kullanıcılar.reverse());
+    }else{
+        res.send(kullanıcılar);
+    }
+ console.log("kullanıcılar sayfasına girildi.");
+
+});
+
+app.get('/users/:id',(req,res)=>{
+    console.log(req.params.id);
+    const kullanici=kullanıcılar.find((kullanici)=>{
+        return kullanici.id===parseInt(req.params.id)
+    });
+    if(kullanici){
+        res.send(kullanici);
+    }else {
+        res.status(404).send( {
+            error:"not found",
+            message:req.params.id+"'li user not found"
+        });
+    }
+   
+});
+
+/*app.get('/users/:yas',(req,res)=>{
+   console.log("sea");
+    const kullanici=kullanıcılar.filter((kullanici)=>{
+        return kullanici.yas<req.params.yas
+    });
+    console.log(kullanici)
+    //res.send(kullanici);
+});*/
+
+
+app.listen(3000,()=>{
+    console.log("Server 3000 portunu dinliyor...");
+});
