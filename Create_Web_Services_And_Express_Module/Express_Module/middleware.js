a5809852859d8abf08701816ca979a54fc0bb1ba
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2021-02-06 14:38:37 +0300

    I added new regulations.

---
+++ Create_Web_Services_And_Express_Module/Express_Module/middleware.js
@@ -0,0 +1,56 @@
+const { response } = require('express');
+const express = require('express');
+const app=express();
+
+
+
+//1. GENEL ARA KATMAN 
+app.use((req,res,next)=>{
+    console.log("1. genel ara katmandayım");
+    let isLogin=false;
+    if(req.url=="/notlogin")
+    isLogin=true; 
+
+    if(isLogin)
+    next();
+    else
+    res.send(`${req.url} middleware çalıştı.<br> Kullanıcı girişi yapılmadı.`);
+});
+
+//2. GENEL ARA KATMAN
+app.use((req,res,next)=>{
+    console.log("2. genel ara katmandayım");
+    next();
+})
+
+//2. ÖZEL ARA KATMAN
+app.use("/login2",(req,res,next)=>{
+    let isLogin=false;
+    if(isLogin)
+    next();
+    else
+    res.send(`${req.url} middlewaree çalıştı.<br> Kullanıcı girişi yapılmadı. (login2)`);
+});
+
+//2. ÖZEL ARA KATMAN
+app.use("/notlogin",(req,res,next)=>{
+   next();
+});
+
+
+app.get("/login",(req,res)=>{
+    res.send("anasayfa");
+});
+app.get("/login2",(req,res)=>{
+    res.send("anasayfa2");
+});
+app.get('/notlogin',(req,res)=>{
+    res.send("you don't have login. This is customer page");
+});
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Server 3000 portunu dinliyor...");
+});
